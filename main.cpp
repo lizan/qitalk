b1de419478ab939df819351ad314056aa6786939
lizan <lizan>
2009-08-10 22:47:37 +0900

    added applicationName

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include <QtGui/QApplication>
+#include <QtGui>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setApplicationName(QObject::tr("Qitalk"));
     MainWindow w;
     w.show();
     return a.exec();
